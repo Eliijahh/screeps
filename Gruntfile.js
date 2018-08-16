80ee1bc85c33b8768f577830c2dfd0ab373c0552
Paolo Caponeri <paolo.caponeri@gmail.com>
2018-08-16 13:34:02 +0100

    Delete weird CLI flags stuff

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,5 @@
 module.exports = function(grunt) {
 
-    var config = require('./.screeps.json')
-    var branch = grunt.option('branch') || config.branch;
-    var email = grunt.option('email') || config.email;
-    var password = grunt.option('password') || config.password;
-    var ptr = grunt.option('ptr') ? true : config.ptr
-
     grunt.loadNpmTasks('grunt-screeps')
     grunt.loadNpmTasks('grunt-contrib-clean')
     grunt.loadNpmTasks('grunt-contrib-copy')
@@ -13,10 +7,10 @@ module.exports = function(grunt) {
     grunt.initConfig({
         screeps: {
             options: {
-                email: email,
-                password: password,
-                branch: branch,
-                ptr: ptr
+                email: "paolo.caponeri@gmail.com",
+                password: "Piergio92!",
+                branch: "default",
+                ptr: false
             },
             dist: {
                 src: ['dist/*.js']
