ebd40673f3c8bb7040969f5a5b01282b9e017cef
Paolo Caponeri <paolo.caponeri@gmail.com>
2018-08-16 13:27:02 +0100

    Include login credential Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,5 @@
 module.exports = function(grunt) {
 
-    var config = require('./.screeps.json')
-    var branch = grunt.option('branch') || config.branch;
-    var email = grunt.option('email') || config.email;
-    var password = grunt.option('password') || config.password;
-    var ptr = grunt.option('ptr') ? true : config.ptr
-
     grunt.loadNpmTasks('grunt-screeps')
     grunt.loadNpmTasks('grunt-contrib-clean')
     grunt.loadNpmTasks('grunt-contrib-copy')
@@ -21,9 +15,9 @@ module.exports = function(grunt) {
     grunt.initConfig({
         screeps: {
             options: {
-                email: email,
-                password: password,
-                branch: branch,
+                email: paolo.caponeri@gmail.com,
+                password: Piergio92!,
+                branch: default,
                 ptr: ptr
             },
             dist: {
