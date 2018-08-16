f99a383e84cfef68f6f22712dc52f51d61276a17
Paolo Caponeri <paolo.caponeri@gmail.com>
2018-08-16 12:43:03 +0100

    Grunt and tern.js

---
+++ Gruntfile.js
@@ -0,0 +1,18 @@
+module.exports = function(grunt) {
+
+    grunt.loadNpmTasks('grunt-screeps');
+
+    grunt.initConfig({
+        screeps: {
+            options: {
+                email: 'paolo.caponeri@gmail.com',
+                password: 'Piergio92!',
+                branch: 'default',
+                ptr: false
+            },
+            dist: {
+                src: ['dist/*.js']
+            }
+        }
+    });
+}
