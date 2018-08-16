517a71680c152796fe77094f8564c31f387f4fe1
Paolo Caponeri <paolo.caponeri@gmail.com>
2018-08-16 14:26:09 +0100

    Test auto-grant 2

--- src/main.js
+++ src/main.js
@@ -4,4 +4,4 @@ if(!Memory.SCRIPT_VERSION || Memory.SCRIPT_VERSION != SCRIPT_VERSION) {
     console.log('New code uploaded. Version: ' + Memory.SCRIPT_VERSION)
 }
 
-console.log("Happy yes!")
+console.log("Happy yes!!")
