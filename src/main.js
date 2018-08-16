b622c0b8421e7288b68a3ce7c4237d09ef3e8912
Paolo Caponeri <paolo.caponeri@gmail.com>
2018-08-16 14:03:56 +0100

    Add auto-upload post-commit

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 require('version')
 if(!Memory.SCRIPT_VERSION || Memory.SCRIPT_VERSION != SCRIPT_VERSION) {
     Memory.SCRIPT_VERSION = SCRIPT_VERSION
-    console.log('New code uplodated')
+    console.log('New code uploaded. Version: ' + Memory.SCRIPT_VERSION)
 }
