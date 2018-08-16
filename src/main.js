fce0b90f11738c1f8ebc7357e01906456b4a68d8
Paolo Caponeri <paolo.caponeri@gmail.com>
2018-08-16 14:19:24 +0100

    Add auto-test grunt

--- src/main.js
+++ src/main.js
@@ -3,3 +3,5 @@ if(!Memory.SCRIPT_VERSION || Memory.SCRIPT_VERSION != SCRIPT_VERSION) {
     Memory.SCRIPT_VERSION = SCRIPT_VERSION
     console.log('New code uploaded. Version: ' + Memory.SCRIPT_VERSION)
 }
+
+console.log("Happy yes!")
