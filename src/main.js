5f61494a993d4733f7e1fc884bf84b60441ecace
Paolo Caponeri <paolo.caponeri@gmail.com>
2018-08-17 16:38:10 +0100

    Fix type in main

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ if(!Memory.SCRIPT_VERSION || Memory.SCRIPT_VERSION != SCRIPT_VERSION) {
     console.log('New code uploaded. Version: ' + Memory.SCRIPT_VERSION)
 }
 
-var role_distributer = require("creeps_role_distributer");
+var role_distributer = require("creeps_roles_distributer");
 var creep_factory = require ("creeps_factory");
 
 //check if new creeps are required and builds them
